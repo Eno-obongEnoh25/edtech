9d6787bf9809abb30e56814cc2dfdcf2f9b6ce4a
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2021-12-23 18:55:42 +0100

    working on url

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -9,7 +9,7 @@ import { ICourse } from './course';
 })
 export class CourseService {
   // private courseUrl = 'assets/course.json';
-  private courseUrl = "{BASE64_ENCODED(uSiUI3kd9MLHngB76b5ER04XfUUZKVem3dIdRUAl:bRPsjdtPzq4hnnYnBR6Y3noDjNJvdrM8l6aDNxtc5uJGXceJcWVVpVw8fCs2L5v94IB5k46RNL1BVG1OThZDoE0iBf8qrf4eAep9EALwPwru53QfWj5Y7uy24S5PkGY5)} https://www.udemy.com/api-2.0/courses/";
+  private courseUrl = 'uSiUI3kd9MLHngB76b5ER04XfUUZKVem3dIdRUAl:bRPsjdtPzq4hnnYnBR6Y3noDjNJvdrM8l6aDNxtc5uJGXceJcWVVpVw8fCs2L5v94IB5k46RNL1BVG1OThZDoE0iBf8qrf4eAep9EALwPwru53QfWj5Y7uy24S5PkGY5 https://www.udemy.com/api-2.0/courses/';
   constructor(private http: HttpClient) { }
 
   getCategory(): Observable<ICourse[]> {
