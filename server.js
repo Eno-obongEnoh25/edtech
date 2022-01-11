e30bce588cd10b712fb89d0fccbf285684470aa7
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2022-01-11 19:55:35 +0100

    nnn

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-// function requireHTTPS(req, res, next) {
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-//       return res.redirect('https://' + req.get('host') + req.url);
-//   }
-//   next();
-// }
-// const express = require('express');
-// const app = express();
-// app.use(requireHTTPS);
-
-// app.use(express.static('./dist/edtech'));
-
-// app.get('/*', function(req, res) {
-//   res.sendFile('index.html', {root: 'dist/edtech/'}
-// );
-// });
-
-// app.listen(process.env.PORT || 8080);
-
-// Add headers
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
   next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/edtech'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/edtech/'}
+);
 });
+
+app.listen(process.env.PORT || 8080);
