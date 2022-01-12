24c22fcc894440f23c581a8ca5f0771365ac8512
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2022-01-12 23:37:17 +0100

    ggg

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -23,7 +23,7 @@ export class CourseService {
         "Content-Type": "application/json;charset=utf-8",
         // client_id: "VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM",
         // client_secret:"v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU",
-      }).set( 'Access-Control-Allow-Origin', "Authorization: Basic {VlBGTnVkTkVJZXJaaDZrWnJxbkFsekNva3FESHFvdFpRNU1KVjlJTTp2M1JSN3RzTm84M2QwSnQyaUdTcndyd3BtSmVhNWxiOGQ5ejJQZ3R2dU1UNldGRExHcHNkbHRlOE12ck1QVk9ZSldoMGRTQ0ZWaDNsVkwzN0ViRkhpYzJmMVkwbjlWZ2phakJGdE5DbjRiNE9zTDQyUk5oYkhxNG9vWVAza0toVQ==(VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU)} https://ud-edtech.herokuapp.com/")
+      }).set( 'Access-Control-Allow-Origin', "Authorization: Basic {VlBGTnVkTkVJZXJaaDZrWnJxbkFsekNva3FESHFvdFpRNU1KVjlJTTp2M1JSN3RzTm84M2QwSnQyaUdTcndyd3BtSmVhNWxiOGQ5ejJQZ3R2dU1UNldGRExHcHNkbHRlOE12ck1QVk9ZSldoMGRTQ0ZWaDNsVkwzN0ViRkhpYzJmMVkwbjlWZ2phakJGdE5DbjRiNE9zTDQyUk5oYkhxNG9vWVAza0toVQ==(VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU)}")
 
           ;
 
