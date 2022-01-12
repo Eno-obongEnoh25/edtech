235064b482ffd4a935ed48903220b59374d6249c
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2022-01-13 00:19:45 +0100

    ooo

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -27,7 +27,7 @@ export class CourseService {
 
 
 
-    let headers = new HttpHeaders().set("Authorization", "Basic {VlBGTnVkTkVJZXJaaDZrWnJxbkFsekNva3FESHFvdFpRNU1KVjlJTTp2M1JSN3RzTm84M2QwSnQyaUdTcndyd3BtSmVhNWxiOGQ5ejJQZ3R2dU1UNldGRExHcHNkbHRlOE12ck1QVk9ZSldoMGRTQ0ZWaDNsVkwzN0ViRkhpYzJmMVkwbjlWZ2phakJGdE5DbjRiNE9zTDQyUk5oYkhxNG9vWVAza0toVQ==(VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU)}");
+    let headers = new HttpHeaders().set("Authorization", "Basic VlBGTnVkTkVJZXJaaDZrWnJxbkFsekNva3FESHFvdFpRNU1KVjlJTTp2M1JSN3RzTm84M2QwSnQyaUdTcndyd3BtSmVhNWxiOGQ5ejJQZ3R2dU1UNldGRExHcHNkbHRlOE12ck1QVk9ZSldoMGRTQ0ZWaDNsVkwzN0ViRkhpYzJmMVkwbjlWZ2phakJGdE5DbjRiNE9zTDQyUk5oYkhxNG9vWVAza0toVQ==");
 
     return this.http.get<ICourse[]>(this.courseUrl, { headers }).pipe(
     // return this.http.get<ICourse[]>(this.courseUrl, { responseType: 'text' as 'json' }).pipe(
