9bb9cb1151cc42c8969202bc744c069c017ef465
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2022-01-12 13:58:45 +0100

    hhh

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -9,16 +9,16 @@ import { ICourse } from './course';
 })
 export class CourseService {
   // private courseUrl = 'assets/course.json';
-  private courseUrl = "BASE64_ENCODED(VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU) https://www.udemy.com/api-2.0/courses/";
+  private courseUrl = "https://www.udemy.com/api-2.0/courses/";
 
   constructor(private http: HttpClient) { }
 
   getCategory(): Observable<ICourse[]> {
 
-    // let headers = new HttpHeaders().set('Authorization', BASE64_ENCODED('VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU'));
+    let headers = new HttpHeaders().set('Authorization', 'Basic ' + btoa('VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU'));
 
-    // return this.http.get<ICourse[]>(this.courseUrl, { headers }).pipe(
-    return this.http.get<ICourse[]>(this.courseUrl, { responseType: 'text' as 'json' }).pipe(
+    return this.http.get<ICourse[]>(this.courseUrl, { headers }).pipe(
+    // return this.http.get<ICourse[]>(this.courseUrl, { responseType: 'text' as 'json' }).pipe(
       tap(data => console.log('All', JSON.stringify(data))),
       catchError(this.handleError)
     );
