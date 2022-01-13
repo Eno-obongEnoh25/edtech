332a75893bee1ae7b6f9a3a1885d5aecbc0a57b9
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2022-01-13 15:14:08 +0100

    added url

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -9,7 +9,7 @@ import { ICourse } from './course';
 })
 export class CourseService {
   // private courseUrl = 'assets/course.json';
-  private courseUrl = "https://udemy.com/api-2.0/courses/";
+  private courseUrl = "VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU https://udemy.com/api-2.0/courses/";
 
   constructor(private http: HttpClient) { }
 
@@ -27,10 +27,10 @@ export class CourseService {
 
 
 
-    let headers = new HttpHeaders().set('Authorization', Base64ENCOCED('VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU'));
+    // let headers = new HttpHeaders().set('Authorization', 'VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU');
 
-    return this.http.get<ICourse[]>(this.courseUrl, { headers }).pipe(
-    // return this.http.get<ICourse[]>(this.courseUrl).pipe(
+    // return this.http.get<ICourse[]>(this.courseUrl, { headers }).pipe(
+    return this.http.get<ICourse[]>(this.courseUrl, { responseType:'text' as 'json' }).pipe(
       tap(data => console.log('All', JSON.stringify(data))),
       catchError(this.handleError)
     );
