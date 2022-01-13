ad6e44b9d7f1f13b6e08501c068e9403c26e3cef
Eno-obongEnoh25 <enoobongenoh25@gmail.com>
2022-01-13 15:05:05 +0100

    added url

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -27,7 +27,7 @@ export class CourseService {
 
 
 
-    let headers = new HttpHeaders().set('Authorization', 'VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU');
+    let headers = new HttpHeaders().set('Authorization', Base64ENCOCED('VPFNudNEIerZh6kZrqnAlzCokqDHqotZQ5MJV9IM:v3RR7tsNo83d0Jt2iGSrwrwpmJea5lb8d9z2PgtvuMT6WFDLGpsdlte8MvrMPVOYJWh0dSCFVh3lVL37EbFHic2f1Y0n9VgjajBFtNCn4b4OsL42RNhbHq4ooYP3kKhU'));
 
     return this.http.get<ICourse[]>(this.courseUrl, { headers }).pipe(
     // return this.http.get<ICourse[]>(this.courseUrl).pipe(
@@ -47,6 +47,10 @@ export class CourseService {
     console.error(errorMessage);
     return throwError(errorMessage);
   }
+}
+
+function Base64ENCOCED(arg0: string): string | string[] {
+  throw new Error('Function not implemented.');
 }
 // function BASE64_ENCODED(arg0: string): string | string[] {
 //   throw new Error('Function not implemented.');
